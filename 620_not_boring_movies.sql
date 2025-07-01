f65c2279df7e667b9e092f41c6239a39449af9ed
dt-japanscript <54771496+dt-japanscript@users.noreply.github.com>
2025-07-01 22:17:26 +0700

    refactor: 620

--- 620_not_boring_movies.sql
+++ 620_not_boring_movies.sql
@@ -1,6 +1,5 @@
 SELECT id, movie, description, rating
 FROM Cinema
-WHERE
-    MOD(id, 2) <> 0
-    AND description != "boring"
-ORDER BY rating desc;
+WHERE id % 2 = 1
+  AND description <> 'boring'
+ORDER BY rating DESC;
