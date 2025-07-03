7d76355d75f919bd0562eb777cf8418eab92c102
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2025-07-03 15:28:05 +0700

    feat: solve 1075. Project Employees I

---
+++ 1075_project_employees_i.sql
@@ -0,0 +1,8 @@
+SELECT
+    project_id ,
+    ROUND(AVG(e.experience_years), 2) AS average_years
+FROM
+    Project p
+    INNER JOIN Employee e
+    ON p.employee_id = e.employee_id
+GROUP BY (project_id)
