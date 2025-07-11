f1d896d6ed127ac5e552952aae5e2f20e6acf671
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2025-07-11 16:06:22 +0700

    refactor: 1633

--- 1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest.sql
@@ -1,9 +1,6 @@
 SELECT
     contest_id,
-    ROUND(COUNT(r.user_id)/(SELECT COUNT(u2.user_id) FROM Users u2) * 100, 2) AS percentage
-FROM
-    Users u
-    JOIN Register r
-    ON r.user_id = u.user_id
+    ROUND(COUNT(user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register
 GROUP BY contest_id
-ORDER BY percentage DESC, contest_id ASC
+ORDER BY percentage DESC, contest_id ASC;
