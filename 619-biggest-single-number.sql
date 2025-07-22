ce57fa9dc3aca91a1e23ca203c15f0e4b750d356
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2025-07-22 21:15:33 +0700

    refactor: 619

--- 619-biggest-single-number.sql
+++ 619-biggest-single-number.sql
@@ -11,3 +11,22 @@ FROM
         HAVING
             COUNT(*) = 1
     ) AS single_numbers;
+
+-- Optimal Method
+SELECT
+    COALESCE(
+        (
+            SELECT
+                num
+            FROM
+                MyNumbers
+            GROUP BY
+                num
+            HAVING
+                COUNT(*) = 1
+            ORDER BY
+                num DESC
+            LIMIT
+                1
+        ), NULL
+    ) AS num;
