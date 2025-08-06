0c9abf8d10b82d59ae6a1fddf2508e3275942e76
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2025-08-06 13:58:48 +0700

    feat: solve 585. Investments in 2016

---
+++ 585-investments-in-2016.sql
@@ -0,0 +1,10 @@
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM (
+    SELECT
+        tiv_2016,
+        COUNT(*) OVER (PARTITION BY tiv_2015) AS tiv_2015_count,
+        COUNT(*) OVER (PARTITION BY lat, lon) AS location_count
+    FROM Insurance
+) t
+WHERE tiv_2015_count > 1
+  AND location_count = 1;
